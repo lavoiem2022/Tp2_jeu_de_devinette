845de414d69c753364863708be393b7dceed646d
lavoiem <lavoiem@educ.college-montreal.qc.ca>
2022-09-30 09:55:28 -0400

    .

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 #Projet jeu de devinette fait par Mehdi Serge Lavoie 406
 import random
 
-boucle_jeu = True
-while boucle_jeu:
-	print("Debut du jeu")
-	boucle_jeu = False
+print("Commencement Du Jeu")
+print(random.randint(1,100)
 
 def on_key_press(self, key, key_modifiers):
