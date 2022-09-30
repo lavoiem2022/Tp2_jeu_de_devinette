0c47109fd9d0444b06808e66f222b5714bfc4f30
lavoiem <lavoiem@educ.college-montreal.qc.ca>
2022-09-30 10:32:54 -0400

    .

--- main.py
+++ main.py
@@ -1,25 +1,22 @@
 #Projet jeu de devinette fait par Mehdi Serge Lavoie 406
 import random
 boucle = True
+
+nombre = random.randint(1, 100)
+print(nombre)
 while boucle:
 
-    nombre = random.randint(1, 100)
 
-def restart():
-    recommence = input("Voulez-vous continuer?")
-    if recommence == ("oui"):
-        return True
-    elif recommence == ("non"):
-        return False
-    return restart()
 
-Start = int(input("Donnez moi un nombre de 1 a 100"))
-if Start > nombre:
-    print("C'est un nombre plus haut")
-if Start < nombre:
-    print("C'est un nombre plus bas")
-if Start ^ nombre:
-    print ("Tu as gagner Bravo!")
+    user_guess = int(input("Donnez moi un nombre de 1 a 100"))
+
+    if user_guess > nombre:
+        print("C'est un nombre plus bas")
+    elif user_guess < nombre:
+        print("C'est un nombre plus haut")
+    else:
+        print ("Tu as gagner Bravo!")
+
 
 
 
