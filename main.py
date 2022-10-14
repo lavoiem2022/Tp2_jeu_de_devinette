6a8cc4887e4e7d6953787bdeda3da12f94c7abe5
lavoiem <lavoiem@educ.college-montreal.qc.ca>
2022-10-14 10:16:10 -0400

    commit ez

--- main.py
+++ main.py
@@ -2,12 +2,19 @@
 import random
 boucle_jeu = True
 nbr_essaies= 1
-nombre = random.randint(1, 100)
+
+borne_minimum = int(input("Choisis les bornes minimum"))
+borne_maximum = int(input("Choisis les bornes maximum"))
+nombre = random.randint(borne_minimum, borne_maximum)
 print(nombre)
+print ("Jai choisis un nombre de ",borne_minimum ," a " ,borne_maximum,"a toi de le deviner")
+
+
 while boucle_jeu:
 
 
-    user_guess = int(input("Donnez moi un nombre de 1 a 100: "))
+
+    user_guess = int(input("Donnez moi un nombre"))
 
     if user_guess > nombre:
         print("C'est un nombre plus bas")
@@ -19,7 +26,10 @@ while boucle_jeu:
         print ("Tu as gagner Bravo!. Tu as réussi en ", nbr_essaies," essaies")
         recommencer = input("Voulez vous recommencer oui ou non")
         if recommencer == "oui":
-            nombre = random.randint(1, 100)
+            borne_minimum = int(input("Choisis les bornes minimum"))
+            borne_maximum = int(input("Choisis les bornes maximum"))
+            print("Jai choisis un nombre de ", borne_minimum, " a ", borne_maximum, "a toi de le deviner")
+            nombre = random.randint(borne_minimum, borne_maximum)
             nbr_essaies = 1
         elif recommencer == "non":
             boucle_jeu = False
